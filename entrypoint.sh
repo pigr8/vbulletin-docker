d6ae604af3a4431a03e1bb5729d0394f563be325
pigr8 <16479888+pigr8@users.noreply.github.com>
2020-04-13 22:34:21 +0200

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -euo pipefail
+
+# Applying desidered PUID to user Nobody
+sed -i 's/:1000:100:/:'$PUID':100:/g' /etc/passwd
+
+# Unset compiled variables for PHP
+unset PHP_MD5 PHP_INI_DIR GPG_KEYS PHP_LDFLAGS PHP_SHA256 \
+      PHPIZE_DEPS PHP_URL PHP_EXTRA_CONFIGURE_ARGS SHLVL \
+      PHP_CFLAGS PHP_ASC_URL PHP_CPPFLAGS SUPERVISOR_ENABLED \
+      SUPERVISOR_PROCESS_NAME SUPERVISOR_GROUP_NAME
+# Unset user variables
+unset DB_HOST DB_NAME DB_USER DB_PASSWORD PUID TZ
+exec "$@"
