a19544dbcb3596854c5c181c5c4768f2bdabef14
pigr8 <16479888+pigr8@users.noreply.github.com>
2020-04-15 14:44:23 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,9 @@ set -euo pipefail
 # Applying desidered PUID to user Nobody
 sed -i 's/:1000:100:/:'$PUID':100:/g' /etc/passwd
 
+# Applying Timezone
+echo "$TZ" >  /etc/timezone
+
 # Unset compiled variables for PHP
 unset PHP_MD5 PHP_INI_DIR GPG_KEYS PHP_LDFLAGS PHP_SHA256 \
       PHPIZE_DEPS PHP_URL PHP_EXTRA_CONFIGURE_ARGS SHLVL \
